67e2536b8d446c743746b3a84f685a38de1661c4
Robert Day <rday@bestdoctor.com>
2018-03-04 06:59:44 -0500

    stubbed analyze impementation.

--- src/commands/analyze.ts
+++ src/commands/analyze.ts
@@ -1,4 +1,17 @@
 import * as vscode from 'vscode';
+const { spawn } = require('child_process');
 
 export default function analyzeCode(document: vscode.TextDocument, context: vscode.ExtensionContext): void { // } Promise<any> {
+    
+    // run the ant command against the file and output the values
+    const buildFile: string = vscode.workspace.rootPath + '\\lib\\PMD\\bin';
+//    const child = spawn('pmd.bat', ['RunPMD'], { cwd: buildFile} );
+    const child = spawn('ant.bat', ['RunPMD'], { cwd: buildFile} );
+
+    child.on('exit', function (code, signal) {
+        console.log('child process exited with ' +
+                    `code ${code} and signal ${signal}`);
+      });
+
+    
 }
